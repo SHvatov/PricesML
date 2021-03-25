21d1c3a062132302faadff0715fefe01580a1e73
MarchenkoVladislav <vlad25091999@mail.ru>
2021-03-25 06:13:20 +0300

    Small refactoring

--- objects-detection/detection/detection.py
+++ objects-detection/detection/detection.py
@@ -1,24 +1,12 @@
 import cv2
 import random
+import pathlib
 
 
 def check_area(area, image_area):
     return area / image_area < 0.95
 
-
-def detect_objects(image_path):
-    res = list()
-    r = random.randint(0, 1000000)
-    for i in range(0, 11, 1):
-        image_path = detect_object(image_path, i + r)
-        if image_path == "":
-            break
-        res.append(image_path)
-
-    return res
-
-
-def detect_object(image_path, i):
+def detect_object(image_path):
     img = cv2.imread(image_path)
     height, width, channels = img.shape
     image_area = height * width
@@ -42,12 +30,9 @@ def detect_object(image_path, i):
     roi = img[y:y + h, x:x + w]
 
     if roi.any():
-        name = 'object' + str(i) + '.jpg'
-        cv2.imwrite(name, roi)
+        output_image_path = str(pathlib.Path().absolute()) + '\object' + str(random.randint(0, 100000)) + '.jpg'
+        cv2.imwrite(output_image_path, roi)
     else:
-        name = ""
+        output_image_path = ""
 
-    return name
-
-
-#print(detect_objects(""))
+    return output_image_path
